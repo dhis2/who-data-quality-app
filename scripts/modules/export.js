c78e18f5d80f82b183bf17d32e30b8a62b130606
Olav Poppe <op.registrering@me.com>
2015-06-01 15:24:20 +0200

    Export - Added basic support for orgunit export and "trend" data.

--- scripts/modules/export.js
+++ scripts/modules/export.js
@@ -4,7 +4,7 @@
 	var app = angular.module('dataExport', []);
 	
 	/**Controller: Parameters*/
-	app.controller("ExportController", function(metaDataService, periodService, BASE_URL) {
+	app.controller("ExportController", function(metaDataService, periodService, requestService, BASE_URL) {
 	    	    
 	    var self = this;
 	    
@@ -38,16 +38,91 @@
 	    	});		    	
 	    }
 	    
+	    self.doOuExport = function() {
+	    	
+	    	
+	    	var requestURL = '/api/organisationUnits.json?fields=name,parent[name,parent[name,parent[name]]]&paging=false&filter=level:eq:' + self.orgunitLevelSelected.level;
+	    	
+	    	//Get data
+	    	requestService.getSingle(requestURL).then(function(response) {
+	    		
+	    		var ouData = response.data.organisationUnits;
+	    		
+	    		var data = [];	    		
+	    		var parent, row;
+	    		for (var i = 0; i < ouData.length; i++) {
+	    			row = [];
+	    			row.push((i+1));
+	    			row.push(ouData[i].name);
+	    			
+	    			parent = ouData[i].parent;
+	    			while (parent) {
+	    				row.push(parent.name);
+	    				parent = parent.parent;
+	    			}
+	    			
+	    			data.push(row);
+	    		}
+	    		
+	    		
+	    		var csvContent = "data:text/csv;charset=utf-8,";
+	    		data.forEach(function(infoArray, index){
+	    		
+	    		   dataString = infoArray.join(";");
+	    		   csvContent += index < data.length ? dataString+ "\n" : dataString;
+	    		
+	    		});
+	    		
+	    		var encodedUri = encodeURI(csvContent);
+	    		window.open(encodedUri);
+	    		
+	    		
+	    		
+	    	});	
+	    	
+	    };
+	    
+	    self.doTrendExport = function() {
+	    	
+	    	var dx;
+	    	if (self.dataElementSelected) {
+	    		dx = self.dataElementSelected.id
+	    	}
+	    	else {
+	    		dx = self.indicatorSelected.id;
+	    	}
+	    	
+	    	var pe = [self.yearSelected.id - 3, self.yearSelected.id - 2, self.yearSelected.id - 1];
+	    	var ou = "LEVEL-" + self.orgunitLevelSelected.level;
+	    	var requestURL = BASE_URL + "/api/analytics.xls?";
+	    	requestURL += "dimension=dx:" + dx;
+	    	requestURL += "&dimension=ou:" + ou;
+	    	requestURL += "&dimension=pe:" + pe.join(';');
+	    	requestURL += "&tableLayout=true&columns=pe&rows=dx;ou";
+	    	
+	    	self.href = requestURL;
+	    	
+	    	window.open(requestURL, '_blank', '');
+	    };
+	    
 	    
 	    self.doExport = function() {
-	    	var dx = self.dataElementSelected.id;
+	    	
+	    	var dx;
+	    	if (self.dataElementSelected) {
+	    		dx = self.dataElementSelected.id
+	    	}
+	    	else {
+	    		dx = self.indicatorSelected.id;
+	    	}
+	    	
 	    	var pe = periodsInYear(self.yearSelected.id).join(';');
 	    	var ou = "LEVEL-" + self.orgunitLevelSelected.level;
 	    	var requestURL = BASE_URL + "/api/analytics.xls?";
-	    	requestURL += "filter=dx:" + dx;
+	    	requestURL += "dimension=dx:" + dx;
 	    	requestURL += "&dimension=ou:" + ou;
 	    	requestURL += "&dimension=pe:" + pe;
-	    	requestURL += "&tableLayout=true&columns=pe&rows=ou";
+	    	requestURL += "&tableLayout=true&columns=pe&rows=dx;ou";
 	    	
 	    	self.href = requestURL;
 	    	
