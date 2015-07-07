e9ca251993b292e7f3db1fe4dfc18abe8d6afad6
Olav Poppe <op.registrering@me.com>
2015-07-07 20:56:56 +0200

    Grunt - additional tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,21 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		copy: {
-      		build: {
+      		core: {
 	        	cwd: 'src',
 	    	    src: [ 'index.html', 'manifest.webapp' ],
     	    	dest: 'build',
         		expand: true
       		},
-			html: {
+			content: {
 				cwd: 'src',
-				src: [ 'module*/*.html'],
+				src: [ 'module*/*.html', 'img/icons/*', 'data/**', 'fonts/**'],
+				dest: 'build',
+				expand: true
+			},
+			concat: {
+				cwd: '.tmp/concat',
+				src: [ '**' ],
 				dest: 'build',
 				expand: true
 			}
@@ -36,12 +42,17 @@ module.exports = function(grunt) {
 				length: 8
 			},
 			images: {
-				src: 'src/img/**/*.{jpg,jpeg,gif,png,webp}',
+				src: 'src/img/*.{jpg,jpeg,gif,png,webp}',
 				dest: 'build/img'
 			}
 		},
 		usemin: {
 			html: 'build/index.html'
+		},
+		uglify: {
+			options: {
+				mangle: false
+			}
 		},
 		zip: {
 			'using-cwd': {
@@ -51,14 +62,29 @@ module.exports = function(grunt) {
 
 			}
 		},
-		uglify: {
+		rsync: {
 			options: {
-				mangle: false
+				args: [""],
+				exclude: ["manifest.webapp"],
+				recursive: true
+			},
+			dev: {
+				options: {
+					src: "build/*",
+					dest: "/usr/local/var/www/apps/dq",
+					delete: true
+				}
 			}
+		},
+		watch: {
+			cwd: 'src',
+			files: ['src/**'],
+			tasks: ['reload-dev']
 		}
 	});
 	
 	// Load the Grunt plugins.
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-usemin');
@@ -67,22 +93,82 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-zip');
+	grunt.loadNpmTasks("grunt-rsync")
 
 	grunt.registerTask(
 		'build',
-		'Compiles all of the assets and copies the files to the build directory.',
+		'Builds app, with minimsed resources',
 		[
 			'clean',
-			'copy:build',
+			'copy:content',
+			'concat-min',
+			'zip',
+			'clean:tmp'
+		]
+	);
+
+	grunt.registerTask(
+		'build-dev',
+		'Builds app, wihtout minimising js or css',
+		[
+			'clean',
+			'copy-build',
+			'concat-only'
+		]
+	);
+
+	grunt.registerTask(
+		'build-dev-zip',
+		'Builds app, wihtout minimising js or css',
+		[
+			'clean',
+			'copy-build',
+			'concat-only',
+			'zip',
+		]
+	);
+
+	grunt.registerTask(
+		'copy-build',
+		'Copies resources (except js and css), move to build',
+		[
+			'copy:core',
+			'copy:content'
+		]
+	);
+
+	grunt.registerTask(
+		'concat-only',
+		'Concat js and css, move to build',
+		[
+			'useminPrepare',
+			'concat:generated',
+			'filerev',
+			'usemin',
+			'copy:concat',
+		]
+	);
+
+	grunt.registerTask(
+		'concat-min',
+		'Concat and minimise js and css, move to build',
+		[
 			'useminPrepare',
 			'concat:generated',
 			'cssmin:generated',
 			'uglify:generated',
 			'filerev',
-			'usemin',
-			'copy:html',
-			'zip',
-			'clean:tmp'
+			'usemin'
 		]
 	);
+
+	grunt.registerTask(
+		'reload-dev',
+		'Make dev build and copy to server',
+		[
+			'build-dev',
+			'rsync:dev'
+		]
+	);
+
 };
