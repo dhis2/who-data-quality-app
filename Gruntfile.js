d80d4557e9d74025bc1e03237b689cbf06ea2e66
Olav Poppe <op.registrering@me.com>
2015-08-28 11:07:41 +0200

    Small restructuring

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       		},
 			content: {
 				cwd: 'src',
-				src: [ 'module*/*.html', 'img/icons/*', 'data/**', 'fonts/**'],
+				src: [ 'module*/*.html', 'app*/*.html', 'img/icons/*', 'data/**', 'fonts/**'],
 				dest: 'build',
 				expand: true
 			},
