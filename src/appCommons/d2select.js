bd050e7d90355148e7b694c42f1e71c722d2a94b
Olav Poppe <op.registrering@me.com>
2015-10-02 11:07:54 +0200

    Added d2Select directive - essentially a wrapper for ui-select

---
+++ src/appCommons/d2select.js
@@ -0,0 +1,30 @@
+(function() {
+
+	var app = angular.module('dataQualityApp');
+
+	app.directive('d2Select', function () {
+		return {
+			scope: {
+				'ngModel': '=',
+				'options': '=',
+				'multiple': '=',
+				'clear': '=',
+				'placeholder': '@',
+				'onSelect': '&'
+			},
+			bindToController: true,
+			controller: "d2SelectController",
+			controllerAs: 'd2sCtrl',
+			templateUrl: 'appCommons/d2Select.html'
+		};
+	});
+
+	app.controller("d2SelectController",
+		[
+			function() {
+				var self = this;
+
+				return self;
+			}]);
+
+})();
