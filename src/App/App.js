c9e846c01ce1af4d43ca0cf9f58490ca1e84b5ad
Erik Arenhill <erik@dhis2.org>
2020-06-22 10:44:32 +0200

    feat: load settings from dataStore

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import { CssVariables } from '@dhis2/ui'
 
+import { DataStoreProvider } from '@dhis2/app-service-datastore'
+
 import {
     Dashboard,
     DataExportForExcel,
@@ -17,46 +19,48 @@ import NoMatch from '../components/NoMatch.js'
 import classes from './App.module.css'
 
 const App = () => (
-    <Router>
-        <CssVariables spacers colors />
-        <div className={classes.container}>
-            <aside className={classes.sidebar}>
-                <SideNav />
-            </aside>
-            <main className={classes.content}>
-                <Switch>
-                    <Route path={PATHS.DASHBOARD} component={Dashboard} />
-                    <Route
-                        exact
-                        path={PATHS.OUTLIERS_AND_MISSING_DATA_ANALYSIS}
-                        component={OutliersAndMissingDataAnalysis}
-                    />
-                    <Route
-                        exact
-                        path={PATHS.CONSISTENCY_ANALYSIS}
-                        component={ConsistencyAnalysis}
-                    />
-                    <Route
-                        exact
-                        path={PATHS.ANNUAL_REPORT}
-                        component={AnnualReport}
-                    />
-                    <Route
-                        exact
-                        path={PATHS.ADMINISTRATION}
-                        component={Administration}
-                    />
-                    <Route
-                        exact
-                        path={PATHS.DATA_EXPORT_FOR_EXCEL}
-                        component={DataExportForExcel}
-                    />
-                    <Redirect exact from="/" to={PATHS.DASHBOARD} />
-                    <Route component={NoMatch} />
-                </Switch>
-            </main>
-        </div>
-    </Router>
+    <DataStoreProvider namespace="dataQualityTool">
+        <Router>
+            <CssVariables spacers colors />
+            <div className={classes.container}>
+                <aside className={classes.sidebar}>
+                    <SideNav />
+                </aside>
+                <main className={classes.content}>
+                    <Switch>
+                        <Route path={PATHS.DASHBOARD} component={Dashboard} />
+                        <Route
+                            exact
+                            path={PATHS.OUTLIERS_AND_MISSING_DATA_ANALYSIS}
+                            component={OutliersAndMissingDataAnalysis}
+                        />
+                        <Route
+                            exact
+                            path={PATHS.CONSISTENCY_ANALYSIS}
+                            component={ConsistencyAnalysis}
+                        />
+                        <Route
+                            exact
+                            path={PATHS.ANNUAL_REPORT}
+                            component={AnnualReport}
+                        />
+                        <Route
+                            exact
+                            path={PATHS.ADMINISTRATION}
+                            component={Administration}
+                        />
+                        <Route
+                            exact
+                            path={PATHS.DATA_EXPORT_FOR_EXCEL}
+                            component={DataExportForExcel}
+                        />
+                        <Redirect exact from="/" to={PATHS.DASHBOARD} />
+                        <Route component={NoMatch} />
+                    </Switch>
+                </main>
+            </div>
+        </Router>
+    </DataStoreProvider>
 )
 
 export default App
