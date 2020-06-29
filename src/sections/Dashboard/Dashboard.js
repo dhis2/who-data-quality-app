47e7ee316e0bb0d74a61dc4ac08af7d4db22b4b5
Hendrik de Graaf <hendrik@dhis2.org>
2020-06-29 14:41:04 +0200

    feat(dashboard): stump out filter component

--- src/sections/Dashboard/Dashboard.js
+++ src/sections/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import i18n from '@dhis2/d2-i18n'
 import cx from 'classnames'
 import { Route, Switch, Redirect } from 'react-router-dom'
@@ -12,6 +12,7 @@ import Outliers from './Outliers.js'
 import TabLink from '../../components/TabLink.js'
 import sectionClasses from '../../components/Section.module.css'
 import { DASHBOARD } from '../../config'
+import Filter from './Filter/Filter.js'
 import classes from './Dashboard.module.css'
 
 const PATHS = {
@@ -21,50 +22,65 @@ const PATHS = {
     OUTLIERS: `${DASHBOARD}/outliers`,
 }
 
-const Dashboard = () => (
-    <>
-        <h1 className={cx([sectionClasses.header], [classes.header])}>
-            <span className={classes.headerText}>{i18n.t('Dashboard')}</span>
-            <Button className={classes.filterButton} primary>
-                Filter
-            </Button>
-        </h1>
-        <Card>
-            <TabBar fixed>
-                <TabLink to={PATHS.COMPLETENESS}>
-                    {i18n.t('Completeness')}
-                </TabLink>
-                <TabLink to={PATHS.CONSISTENCY_TIME}>
-                    {i18n.t('Consistency - time')}
-                </TabLink>
-                <TabLink to={PATHS.CONSISTENCY_DATA}>
-                    {i18n.t('Consistency - data')}
-                </TabLink>
-                <TabLink to={PATHS.OUTLIERS}>{i18n.t('Outliers')}</TabLink>
-            </TabBar>
-            <div className={sectionClasses.content}>
-                <Switch>
-                    <Route
-                        exact
-                        path={PATHS.COMPLETENESS}
-                        component={Completeness}
-                    />
-                    <Route
-                        exact
-                        path={PATHS.CONSISTENCY_TIME}
-                        component={ConsistencyTime}
-                    />
-                    <Route
-                        exact
-                        path={PATHS.CONSISTENCY_DATA}
-                        component={ConsistencyData}
-                    />
-                    <Route exact path={PATHS.OUTLIERS} component={Outliers} />
-                    <Redirect from={DASHBOARD} to={PATHS.COMPLETENESS} />
-                </Switch>
-            </div>
-        </Card>
-    </>
-)
+const Dashboard = () => {
+    const [showFilter, setShowFilter] = useState(false)
+
+    return (
+        <>
+            <h1 className={cx([sectionClasses.header], [classes.header])}>
+                <span className={classes.headerText}>
+                    {i18n.t('Dashboard')}
+                </span>
+                <Button
+                    className={classes.filterButton}
+                    onClick={() => setShowFilter(!showFilter)}
+                    primary
+                >
+                    {i18n.t('Filter')}
+                </Button>
+            </h1>
+            {showFilter && <Filter />}
+            <Card>
+                <TabBar fixed>
+                    <TabLink to={PATHS.COMPLETENESS}>
+                        {i18n.t('Completeness')}
+                    </TabLink>
+                    <TabLink to={PATHS.CONSISTENCY_TIME}>
+                        {i18n.t('Consistency - time')}
+                    </TabLink>
+                    <TabLink to={PATHS.CONSISTENCY_DATA}>
+                        {i18n.t('Consistency - data')}
+                    </TabLink>
+                    <TabLink to={PATHS.OUTLIERS}>{i18n.t('Outliers')}</TabLink>
+                </TabBar>
+                <div className={sectionClasses.content}>
+                    <Switch>
+                        <Route
+                            exact
+                            path={PATHS.COMPLETENESS}
+                            component={Completeness}
+                        />
+                        <Route
+                            exact
+                            path={PATHS.CONSISTENCY_TIME}
+                            component={ConsistencyTime}
+                        />
+                        <Route
+                            exact
+                            path={PATHS.CONSISTENCY_DATA}
+                            component={ConsistencyData}
+                        />
+                        <Route
+                            exact
+                            path={PATHS.OUTLIERS}
+                            component={Outliers}
+                        />
+                        <Redirect from={DASHBOARD} to={PATHS.COMPLETENESS} />
+                    </Switch>
+                </div>
+            </Card>
+        </>
+    )
+}
 
 export default Dashboard
---
+++ src/sections/Dashboard/Filter/Filter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import propTypes from '@dhis2/prop-types'
+
+const Filter = () => {
+    return <div>I will become the filter</div>
+}
+
+// Filter.propTypes = {}
+
+export default Filter
