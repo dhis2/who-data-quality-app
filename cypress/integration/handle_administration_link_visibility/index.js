c49d805c6126091cc6016875be15cdc4bc6691c4
Erik A <erik@dhis2.org>
2020-07-02 11:24:30 +0200

    feat: show/hide admin menu depending on users authority (#101)
    * feat: show/hide admin menu depending on authority
    
    * refactor: cleanup authority- check
    
    * test: add cypress tests for show/hide admin-menu
    
    * test: make sure unfetch is always used in cypress
    
    * test(networkshim): support different dhis2_core urls
    
    * chore(networkshim): produce updated network fixtures without host
    
    * refactor: use local fixture for response
    
    * chore: remove file not used
    
    * refactor: use some for better performance
    
    * test: improve scenario descriptions
    
    Co-authored-by: Hendrik de Graaf <hendrik@dhis2.org>

---
+++ cypress/integration/handle_administration_link_visibility/index.js
@@ -0,0 +1,70 @@
+import { Before, Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+
+const fixture = {
+    name: 'Cypress',
+    authorities: [],
+    settings: {
+        keyUiLocale: 'en',
+    },
+}
+
+Before(() => {
+    cy.server()
+})
+
+Given('the user has authority ALL and F_INDICATOR_PUBLIC_ADD', () => {
+    cy.route({
+        url: `${Cypress.env('dhis2_base_url')}/api/me`,
+        response: {
+            ...fixture,
+            authorities: ['ALL', 'F_INDICATOR_PUBLIC_ADD'],
+        },
+    })
+})
+
+Given('the user has authority F_INDICATOR_PUBLIC_ADD', () => {
+    cy.route({
+        url: `${Cypress.env('dhis2_base_url')}/api/me`,
+        response: {
+            ...fixture,
+            authorities: ['F_INDICATOR_PUBLIC_ADD'],
+        },
+    })
+})
+
+Given('the user has authority ALL', () => {
+    cy.route({
+        url: `${Cypress.env('dhis2_base_url')}/api/me`,
+        response: {
+            ...fixture,
+            authorities: ['ALL'],
+        },
+    })
+})
+
+Given('the user does not have authority to administer the app', () => {
+    cy.route({
+        url: `${Cypress.env('dhis2_base_url')}/api/32/me`,
+        method: 'GET',
+        response: {
+            ...fixture,
+            authorities: [],
+        },
+    })
+})
+
+When('the user accesses any page', () => {
+    cy.visit('/#')
+})
+
+Then('the administration link in the menu is visible', () => {
+    cy.get('[data-test="who-sidenav"] span.label')
+        .contains('Administration')
+        .should('exist')
+})
+
+Then('the administration link in the menu is hidden', () => {
+    cy.get('[data-test="who-sidenav"] span.label')
+        .contains('Administration')
+        .should('not.exist')
+})
