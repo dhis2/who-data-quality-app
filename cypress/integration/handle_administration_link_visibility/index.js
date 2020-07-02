f55eb38d557431ec9e0ec115c9b4b774aff20fa5
Erik <erik@dhis2.org>
2020-07-02 10:46:34 +0200

    refactor: use local fixture for response

--- cypress/integration/handle_administration_link_visibility/index.js
+++ cypress/integration/handle_administration_link_visibility/index.js
@@ -1,55 +1,55 @@
 import { Before, Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
+const fixture = {
+    name: 'Cypress',
+    authorities: [],
+    settings: {
+        keyUiLocale: 'en',
+    },
+}
+
 Before(() => {
     cy.server()
 })
 
 Given('the user has authority ALL or F_INDICATOR_PUBLIC_ADD', () => {
-    cy.fixture('SideNav/me').then(fx => {
-        cy.route({
-            url: `${Cypress.env('dhis2_base_url')}/api/me`,
-            response: {
-                ...fx,
-                authorities: ['ALL', 'F_INDICATOR_PUBLIC_ADD'],
-            },
-        })
+    cy.route({
+        url: `${Cypress.env('dhis2_base_url')}/api/me`,
+        response: {
+            ...fixture,
+            authorities: ['ALL', 'F_INDICATOR_PUBLIC_ADD'],
+        },
     })
 })
 
 Given('the user has authority F_INDICATOR_PUBLIC_ADD', () => {
-    cy.fixture('SideNav/me').then(fx => {
-        cy.route({
-            url: `${Cypress.env('dhis2_base_url')}/api/me`,
-            response: {
-                ...fx,
-                authorities: ['F_INDICATOR_PUBLIC_ADD'],
-            },
-        })
+    cy.route({
+        url: `${Cypress.env('dhis2_base_url')}/api/me`,
+        response: {
+            ...fixture,
+            authorities: ['F_INDICATOR_PUBLIC_ADD'],
+        },
     })
 })
 
 Given('the user has authority ALL', () => {
-    cy.fixture('SideNav/me').then(fx => {
-        cy.route({
-            url: `${Cypress.env('dhis2_base_url')}/api/me`,
-            response: {
-                ...fx,
-                authorities: ['ALL'],
-            },
-        })
+    cy.route({
+        url: `${Cypress.env('dhis2_base_url')}/api/me`,
+        response: {
+            ...fixture,
+            authorities: ['ALL'],
+        },
     })
 })
 
 Given('the user does not have authority to administer the app', () => {
-    cy.fixture('SideNav/me').then(fx => {
-        cy.route({
-            url: `${Cypress.env('dhis2_base_url')}/api/32/me`,
-            method: 'GET',
-            response: {
-                ...fx,
-                authorities: [],
-            },
-        })
+    cy.route({
+        url: `${Cypress.env('dhis2_base_url')}/api/32/me`,
+        method: 'GET',
+        response: {
+            ...fixture,
+            authorities: [],
+        },
     })
 })
 
