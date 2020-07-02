8e717fe632b1bb906baf9d952c04db26c189eb85
Erik <erik@dhis2.org>
2020-07-02 15:54:14 +0200

    refactor: move router to own component

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,10 @@
 import React from 'react'
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import { CssVariables } from '@dhis2/ui'
 
 import { DataStoreProvider } from '@dhis2/app-service-datastore'
 
 import { AppContextProvider } from './AppContext'
-
-import {
-    Dashboard,
-    DataExportForExcel,
-    OutliersAndMissingDataAnalysis,
-    ConsistencyAnalysis,
-    AnnualReport,
-    Administration,
-} from '../sections'
-
-import { PATHS, ADMIN_AUTHORITIES } from '../config'
-import SideNav from '../components/SideNav.js'
-import NoMatch from '../components/NoMatch.js'
-import classes from './App.module.css'
+import AppRouter from './AppRouter'
+import { ADMIN_AUTHORITIES } from '../config'
 import useAuthorites from '../hooks/useAuthorites'
 
 const App = () => {
@@ -33,51 +19,7 @@ const App = () => {
     return (
         <AppContextProvider value={{ currentUserIsAdmin: hasAnyAuthority }}>
             <DataStoreProvider namespace="dataQualityTool">
-                <Router>
-                    <CssVariables spacers colors />
-                    <div className={classes.container}>
-                        <aside className={classes.sidebar}>
-                            <SideNav />
-                        </aside>
-                        <main className={classes.content}>
-                            <Switch>
-                                <Route
-                                    path={PATHS.DASHBOARD}
-                                    component={Dashboard}
-                                />
-                                <Route
-                                    exact
-                                    path={
-                                        PATHS.OUTLIERS_AND_MISSING_DATA_ANALYSIS
-                                    }
-                                    component={OutliersAndMissingDataAnalysis}
-                                />
-                                <Route
-                                    exact
-                                    path={PATHS.CONSISTENCY_ANALYSIS}
-                                    component={ConsistencyAnalysis}
-                                />
-                                <Route
-                                    exact
-                                    path={PATHS.ANNUAL_REPORT}
-                                    component={AnnualReport}
-                                />
-                                <Route
-                                    exact
-                                    path={PATHS.ADMINISTRATION}
-                                    component={Administration}
-                                />
-                                <Route
-                                    exact
-                                    path={PATHS.DATA_EXPORT_FOR_EXCEL}
-                                    component={DataExportForExcel}
-                                />
-                                <Redirect exact from="/" to={PATHS.DASHBOARD} />
-                                <Route component={NoMatch} />
-                            </Switch>
-                        </main>
-                    </div>
-                </Router>
+                <AppRouter />
             </DataStoreProvider>
         </AppContextProvider>
     )
